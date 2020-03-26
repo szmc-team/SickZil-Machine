e64c0eb86269c68d04c907f2a77cd48625faed2a
merong <merong0704@gmail.com>
2020-03-27 02:40:51 +0900

    타이핑 테스트 및 예시코드 추가

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "typeface-roboto";
+import "./modules";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
