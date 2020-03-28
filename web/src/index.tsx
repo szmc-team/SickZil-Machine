71cf7fedf5672d31b66f46ca8148819415ae9f12
HyunnoH <43626722+HyunnoH@users.noreply.github.com>
2020-03-28 18:09:00 +0900

    리덕스 스토어 제작

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,21 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import { MemoryRouter } from 'react-router-dom'
 import { SnackbarProvider } from 'notistack'
 import App from './features/app'
 import * as serviceWorker from './serviceWorker'
 import 'typeface-roboto'
 import { GlobalStyle, ThemeProvider } from './styles'
+import rootReducer from './stores/'
+
+const store = createStore(rootReducer)
 
 const Root: React.FC = () => (
-  <MemoryRouter>
-    <SnackbarProvider>
-      <ThemeProvider>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
-    </SnackbarProvider>
-  </MemoryRouter>
+  <Provider store={store}>
+    <MemoryRouter>
+      <SnackbarProvider>
+        <ThemeProvider>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
+      </SnackbarProvider>
+    </MemoryRouter>
+  </Provider>
 )
 
 ReactDOM.render(<Root />, document.getElementById('root'))
