1f32c73d0b78c9fbfc359071495777174130489f
HyunnoH <43626722+HyunnoH@users.noreply.github.com>
2020-03-28 18:11:34 +0900

    Revert "리덕스 스토어 제작"
    This reverts commit 71cf7fedf5672d31b66f46ca8148819415ae9f12.

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,28 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
 import { MemoryRouter } from 'react-router-dom'
 import { SnackbarProvider } from 'notistack'
 import App from './features/app'
 import * as serviceWorker from './serviceWorker'
 import 'typeface-roboto'
 import { GlobalStyle, ThemeProvider } from './styles'
-import rootReducer from './stores/'
-
-const store = createStore(rootReducer)
 
 const Root: React.FC = () => (
-  <Provider store={store}>
-    <MemoryRouter>
-      <SnackbarProvider>
-        <ThemeProvider>
-          <GlobalStyle />
-          <App />
-        </ThemeProvider>
-      </SnackbarProvider>
-    </MemoryRouter>
-  </Provider>
+  <MemoryRouter>
+    <SnackbarProvider>
+      <ThemeProvider>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </SnackbarProvider>
+  </MemoryRouter>
 )
 
 ReactDOM.render(<Root />, document.getElementById('root'))
--- web/src/stores/configureStores.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import global from './modules/global'
-
-const rootReducer = combineReducers({
-  global,
-})
-
-export default rootReducer
