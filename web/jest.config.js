18b9bc7a8f3c22b64bbe1f3467ff7727bb3e77be
kur <rhdnfka94@gmail.com>
2020-03-29 17:09:29 +0900

    mock을 이용한 테스트 준비
    프로젝트 설정: jest로 실행할 때 *.png 파일 import할 수 있게 하기.
    mock 폴더 생성

--- web/jest.config.js
+++ web/jest.config.js
@@ -11,6 +11,12 @@ module.exports = {
   preset: 'ts-jest',
   // All imported modules in your tests should be mocked automatically
   // automock: false,
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+    "<rootDir>/assetsTransformer.js",
+    "\\.(css|less)$":
+    "<rootDir>/assetsTransformer.js"
+  },
 
   // Stop running tests after `n` failures
   // bail: 0,
