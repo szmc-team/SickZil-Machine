506c88ace1e04956ec18ca0fd39e3b38c3f275be
kur <rhdnfka94@gmail.com>
2020-03-31 21:19:59 +0900

    snet 모델 로드하기(WIP)

--- web/src/core/neural-net/NeuralNet.ts
+++ web/src/core/neural-net/NeuralNet.ts
@@ -1,5 +1,21 @@
 import * as tf from '@tensorflow/tfjs'
+//import snet from '../../../public/snet/model.json'
+//import snet from '~/../public/snet/model.json'
 
-const tmp = () => tf.ones([1, 2, 3])
+const tmp = async () => {
+  //console.log(snet)
+  tf.ones([1, 2, 3]).print()
+  
+  try {
+    const model = await tf.loadLayersModel(
+      //'https://localhost:3000/snet/model.json')
+    'file://public/snet/model.json')
+    console.log(model)
+    console.log(typeof model)
+  }
+  catch(err) {
+    console.log('failed:', err)
+  }
+}
 
 export default tmp
