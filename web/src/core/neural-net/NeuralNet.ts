499d498a3d09be9d5275766fea818cb319b48244
merong <merong0704@gmail.com>
2020-04-01 21:24:03 +0900

    테스트 출력 개선

--- web/src/core/neural-net/NeuralNet.ts
+++ web/src/core/neural-net/NeuralNet.ts
@@ -5,8 +5,6 @@ const tmp = async () => {
 
   try {
     const model = await tf.loadLayersModel('file://public/snet/model.json')
-
-    console.log(model)
     console.log(typeof model)
   } catch (err) {
     console.log('failed:', err)
