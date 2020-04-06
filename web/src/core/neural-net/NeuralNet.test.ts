6172978c283be5a54325f748b4b307e66cef80c4
kur <rhdnfka94@gmail.com>
2020-04-06 21:07:31 +0900

    maskGen 테스트 케이스 추가, 몇몇 mock 이미지 이름 변경

--- web/src/core/neural-net/NeuralNet.test.ts
+++ web/src/core/neural-net/NeuralNet.test.ts
@@ -12,12 +12,32 @@ const readPng = (path: string) => {
   return PNG.sync.read(buf)
 }
 
-test('inference small 16x image', async () => {
-  const png = readPng('./src/mocks/small16x.png')
+test('inference tiny 16x image', async () => {
+  const png = readPng('./src/mocks/tiny16x.png')
   const inpArr = new Int32Array(png.data)
   const outArr = await genMask(inpArr, png.width, png.height)
 
   const outTensor = tf.tensor(outArr, [png.height, png.width, 3]) as tf.Tensor3D
   const pngArr = await tf.node.encodePng(outTensor)
-  fs.outputFileSync('./output/check.png', Buffer.from(pngArr), 'binary')
+  fs.outputFileSync('./output/tiny16x.png', Buffer.from(pngArr), 'binary')
+})
+
+test('inference 16x image', async () => {
+  const png = readPng('./src/mocks/middle16x.png')
+  const inpArr = new Int32Array(png.data)
+  const outArr = await genMask(inpArr, png.width, png.height)
+
+  const outTensor = tf.tensor(outArr, [png.height, png.width, 3]) as tf.Tensor3D
+  const pngArr = await tf.node.encodePng(outTensor)
+  fs.outputFileSync('./output/middle16x.png', Buffer.from(pngArr), 'binary')
+})
+
+test('inference unpadded image', async () => {
+  const png = readPng('./src/mocks/middle.png')
+  const inpArr = new Int32Array(png.data)
+  const outArr = await genMask(inpArr, png.width, png.height)
+
+  const outTensor = tf.tensor(outArr, [png.height, png.width, 3]) as tf.Tensor3D
+  const pngArr = await tf.node.encodePng(outTensor)
+  fs.outputFileSync('./output/midddle.png', Buffer.from(pngArr), 'binary')
 })
