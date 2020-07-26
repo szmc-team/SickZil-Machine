bb4e0c5d318fd6612b3adeb482155d79b74a01e1
HyunnoH <hyunnoh01@gmail.com>
2020-07-26 05:41:43 +0000

    Add drawing feature

--- web/src/features/editor/Editor.tsx
+++ web/src/features/editor/Editor.tsx
@@ -1,13 +1,14 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core'
 import { useState, useEffect, useRef, useCallback } from 'react'
-import { drawLine, Position2D } from './draw'
+import { Position2D } from './draw'
 import { useEditorState } from '~/store/modules/editor'
 import { useFileEntryQuery, useFileEntryLazyQuery } from '~/graphql'
 import Konva from 'konva'
 import { Stage, Layer, Image } from 'react-konva'
 import { KonvaEventObject } from 'konva/types/Node'
 import useImage from 'use-image'
+import { Line } from 'konva/types/shapes/Line'
 
 const Editor: React.FC = () => {
   const { fileEntryId } = useEditorState()
@@ -20,12 +21,18 @@ const Editor: React.FC = () => {
   const img = data?.fileEntry?.url
   const [image] = useImage(img || '')
 
+  const [dimension, setDemension] = useState<{ width: number; height: number }>(
+    { width: image?.height || 0, height: image?.height || 0 }
+  )
+
+  console.log(image)
   const [context, setContext] = useState<CanvasRenderingContext2D | null>(null)
   const [isDrawing, setIsDrawing] = useState<boolean>(false)
   const [rect, setRect] = useState<DOMRect | null>(null)
 
-  const prev = useRef<Position2D>({ x: 0, y: 0 })
-  const curr = useRef<Position2D>({ x: 0, y: 0 })
+  const lastLine = useRef<Line>()
+
+  const stageRef = useRef<Stage>(null)
   const canvasRef = useRef<HTMLCanvasElement>(null)
   const layerRef = useRef<Konva.Layer>(null)
 
@@ -38,31 +45,32 @@ const Editor: React.FC = () => {
 
   const startDrawing = useCallback(
     (e: KonvaEventObject<MouseEvent>) => {
-      const stage = e.target.getStage()
-      if (!stage) return
-      const pos = stage.getPointerPosition()
+      if (!stageRef.current || !layerRef.current) return
+      setIsDrawing(true)
+      const pos = stageRef.current.getStage().getPointerPosition()
       if (!pos) return
-      const lastLine = new Konva.Line({
+
+      lastLine.current = new Konva.Line({
         stroke: 'red',
         strokeWidth: 5,
         globalCompositeOperation: 'source-over',
         points: [pos.x, pos.y],
       })
-      layerRef.current?.add(lastLine)
-      // e.currentTarget.getLayer()?.add(lastLine)
+      layerRef.current.add(lastLine.current)
     },
     [rect]
   )
 
   const draw = useCallback(
     (e: KonvaEventObject<MouseEvent>) => {
-      if (!rect) return
-      if (context && isDrawing) {
-        prev.current = curr.current
-        curr.current = { ...e.target.getStage()?.getPointerPosition() }
+      if (!stageRef.current || !isDrawing || !lastLine.current) return
 
-        drawLine(context, prev.current, curr.current)
-      }
+      const pos = stageRef.current?.getStage().getPointerPosition()
+      if (!pos) return
+
+      const newPoints = lastLine.current.points().concat([pos.x, pos.y])
+      lastLine.current.points(newPoints)
+      layerRef.current?.batchDraw()
     },
     [context, rect, isDrawing]
   )
@@ -81,10 +89,12 @@ const Editor: React.FC = () => {
         <Stage
           onMouseDown={startDrawing}
           onMouseMove={draw}
+          onMouseUp={stopDrawing}
           width={window.innerWidth}
           height={window.innerHeight}
+          ref={stageRef}
         >
-          <Layer>
+          <Layer x={123}>
             <Image image={image} />
           </Layer>
           <Layer ref={layerRef} />
